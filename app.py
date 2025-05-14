964762488f65a82b49bcccf0b231914df551ae21
Baji1718 <bajishaik31518@gmail.com>
2025-05-14 07:10:42 +0000

    new1

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
+# from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # 🔥 Enable CORS for all routes
+# CORS(app)  # 🔥 Enable CORS for all routes
 
 materials = [
     {
