a1e0088613faeef9fe2fe06a46e08cd0918756ad
Baji1718 <bajishaik31518@gmail.com>
2025-05-14 09:34:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # 🔥 Enable CORS for all routes
+
+materials = [
+    {
+        "MaterialName": "Vanilla Extract",
+        "UOM": "Ltr",
+        "Description": "Used in vanilla ice cream"
+    },
+    {
+        "MaterialName": "Waffle Cone",
+        "UOM": "Pcs",
+        "Description": "Cone for serving ice cream"
+    }
+    ,{
+        "MaterialName": "Vanilla Extract",
+        "UOM": "Ltr",
+        "Description": "Used in vanilla ice cream"
+    },
+    {
+        "MaterialName": "Waffle Cone",
+        "UOM": "Pcs",
+        "Description": "Cone for serving ice cream"
+    },
+    {
+        "MaterialName": "Vanilla Extract",
+        "UOM": "Ltr",
+        "Description": "Used in vanilla ice cream"
+    },
+    {
+        "MaterialName": "Waffle Cone",
+        "UOM": "Pcs",
+        "Description": "Cone for serving ice cream"
+    }
+]
+
+@app.route('/api/materials', methods=['GET'])
+def get_materials():
+    return jsonify(materials)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
