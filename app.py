48d725d5999a7df9b0a307365dbaa864b0a59273
Baji1718 <bajishaik31518@gmail.com>
2025-05-15 06:54:53 +0000

    new change01

--- app.py
+++ app.py
@@ -36,21 +36,10 @@ materials = [
 def get_materials():
     return jsonify(materials)
 
-# if __name__ == '__main__':
-#     import os
-#     port = int(os.environ.get('PORT', 5000))
-#     app.run(host='0.0.0.0', port=port)
-
 if __name__ == '__main__':
     import os
-    import webbrowser
-    from threading import Timer
-
     port = int(os.environ.get('PORT', 5000))
-    url = f"https://baji1718.github.io/Siva/APP.html"
-
-    # Open the browser after a short delay
-    Timer(1, lambda: webbrowser.open(url)).start()
-
     app.run(host='0.0.0.0', port=port)
+
+
 
